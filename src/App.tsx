20da460b56d950ee7928efae67ad805db3cb5567
Leandro <leandrodbdias@gmail.com>
2020-04-27 20:07:34 -0300

    Melhoria da estrutura do projeto em layouts e styles separados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Main from './pages/main';
 
-import './styles/global.css'
+import LayoutMain from './layout/main';
 
 export default function App() {
-  return <Main />
+  return <LayoutMain />
 }
---
+++ src/layout/main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Main from '../../pages/main';
+
+import '../../styles/global.css'
+
+export default function LayoutMain() {
+  return <Main />
+}
