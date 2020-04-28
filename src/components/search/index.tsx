165bcdcd6c7780d155a8ae79de814ba60be5ca48
Leandro <leandrodbdias@gmail.com>
2020-04-27 21:10:18 -0300

    Estilização da barra de procura

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import './style.css'
+
 export default function Search() {
-  return <input />
+  return (
+    <input className="search" placeholder="Digite o que está procurando..." type="text" />
+  )
 }
