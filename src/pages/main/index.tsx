089ff06131f022f87510c90527088d57f2660c13
devleandrodias <leandrodbdias@gmail.com>
2020-05-10 15:49:07 -0700

    Implementação dos reacts hooks e busca a API de tools/

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../../components/search';
 import Checkbox from '../../components/checkbox';
 import Button from '../../components/button';
 import Card from '../../components/card';
+import api from '../../services/api';
 
 import './style.css';
 
-const Main: React.FC = () => (
-  <>
-    <h1>VUTTR</h1>
-    <h2>Very Useful Tools to Remember</h2>
-    <div className="group-search">
-      <Search />
-      <Checkbox />
-      <Button />
-    </div>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </>
-);
+interface Result {
+  message: string;
+  success: boolean;
+  data: [Tool];
+  error: string;
+}
+
+interface Tool {
+  _id: string;
+  description: string;
+  link: string;
+  tags: [string];
+  title: string;
+}
+
+const Main: React.FC = () => {
+  const [tools, setTools] = useState<Result | null>(null);
+
+  useEffect(() => {
+    async function loadTools(): Promise<void> {
+      const response = await api.get('tools');
+      const tool: Result = response.data;
+
+      setTools(tool);
+    }
+
+    loadTools();
+  }, []);
+
+  return (
+    <>
+      <h1>VUTTR</h1>
+      <h2>Very Useful Tools to Remember</h2>
+      <div className="group-search">
+        <Search />
+        <Checkbox />
+        <Button />
+      </div>
+      {tools ? <Card /> : <h1>Carregando</h1>}
+    </>
+  );
+};
 
 export default Main;
